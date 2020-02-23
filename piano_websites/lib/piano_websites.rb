0a4914ea95ab8d6c1f3e661b825096707e211792
clean-command-1851 <ed7901979t@gmail.com>
2020-02-23 16:42:27 +0000

    Automatically backed up by Learn

--- piano_websites/lib/piano_websites.rb
+++
@@ -1,6 +0,0 @@
-require "piano_websites/version"
-
-module PianoWebsites
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- spec/piano_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Piano do
-  it "has a version number" do
-    expect(Piano::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
